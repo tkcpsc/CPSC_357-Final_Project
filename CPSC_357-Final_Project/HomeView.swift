bdb8b5aae2339378c6ad660bb1cc4fadd0d47562
Thomas Kudey <thomskudey@gmail.com>
2024-12-09 06:04:52 -0800

    added image logo

--- CPSC_357-Final_Project/HomeView.swift
+++ CPSC_357-Final_Project/HomeView.swift
@@ -5,6 +5,7 @@
 //  Created by Tommy Kudey on 12/5/24.
 //
 
+
 import SwiftUI
 
 struct HomeView: View {
@@ -13,6 +14,12 @@ struct HomeView: View {
             Text("Welcome to GolfBuddy Lite!")
                 .font(.title)
                 .multilineTextAlignment(.center)
+            
+            Image("Image")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300) // Adjust the size as needed
+            
             Text("Track your golf practice and find nearby ranges.")
                 .font(.subheadline)
                 .multilineTextAlignment(.center)
